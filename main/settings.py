aff6a7a5636cf3b7a8df1c58996789f5b6cb072a
BreadDev <breadydev@gmail.com>
2023-12-24 13:04:31 +0600

    Access token lifespan changed from 60 to 5 minutes for test

--- main/settings.py
+++ main/settings.py
@@ -116,7 +116,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
     "SLIDING_TOKEN_LIFETIME": timedelta(days=1),
     "SLIDING_TOKEN_REFRESH_LIFETIME_GRACE_PERIOD": timedelta(days=2),
