40d79d1fbedc95bc4ddac09840edc972af977700
BreadlyDev <breadydev@gmail.com>
2025-09-25 22:37:55 +0200

    added logger setup function

---
+++ new-version/lib/logger/logger.go
@@ -0,0 +1,33 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envDev:
+		log = slog.New(slog.NewTextHandler(
+			os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug},
+		))
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(
+			os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo},
+		))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(
+			os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo},
+		))
+	}
+
+	return log
+}
