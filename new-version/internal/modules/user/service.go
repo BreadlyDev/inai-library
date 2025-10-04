95dd87d450a3d5f84bd0f34a2f24d00d11d12e8b
BreadlyDev <breadydev@gmail.com>
2025-10-04 21:39:03 +0200

    fixed 'Register' and 'Login' functions

--- new-version/internal/modules/user/service.go
+++ new-version/internal/modules/user/service.go
@@ -3,120 +3,34 @@ package user
 import (
 	"context"
 	"fmt"
-	"log"
 	"log/slog"
 	"new-version/internal/config"
-	"time"
 
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
-	"golang.org/x/crypto/bcrypt"
 )
 
-type AuthService interface {
-	HashPass(pass string) string
-	ComparePassword(hashPass string, pass string) (bool, error)
-	GenJwtToken(userIn UserLogin) (string, error)
-}
-
-type JwtAuthService struct {
-	log *slog.Logger
-	cfg *config.Security
-}
-
-func NewJwtAuthService(log *slog.Logger, cfg *config.Security) *JwtAuthService {
-	return &JwtAuthService{log: log, cfg: cfg}
-}
-
-func (j *JwtAuthService) HashPass(pass string) (string, error) {
-	const op = "modules.user.service.HashPass"
-
-	h, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
-	if err != nil {
-		return "", fmt.Errorf("%s: %w", op, err)
-	}
-
-	return string(h), nil
-}
-
-func (j *JwtAuthService) ComparePassword(hashPass string, pass string) (bool, error) {
-	const op = "modules.user.service.ComparePassword"
-
-	if err := bcrypt.CompareHashAndPassword([]byte(hashPass), []byte(pass)); err != nil {
-		if err == bcrypt.ErrMismatchedHashAndPassword {
-			return false, fmt.Errorf("%s: wrong password", op)
-		}
-
-		return false, fmt.Errorf("%s: %w", op, err)
-	}
-
-	return true, nil
-}
-
-func (j *JwtAuthService) GenJwtToken(userIn UserLogin) (string, error) {
-	const op = "modules.user.service.GenJwtToken"
-
-	claims := jwt.MapClaims{
-		"sub":          userIn.Email,
-		"access_level": userIn.AccessLevel,
-		"exp":          time.Now().Add(j.cfg.AccessTokenExpire).Unix(),
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	signedToken, err := token.SignedString(j.cfg.JwtSecret)
-	if err != nil {
-		return "", fmt.Errorf("%s: %w", op, err)
-	}
-
-	return signedToken, nil
-}
-
-func ValidateJwt(cfg *config.Security, signedToken string) (jwt.Claims, error) {
-	const op = "modules.user.service.ValidateJwt"
-
-	parsedToken, err := jwt.Parse(signedToken, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("%s: unexpected signing method", op)
-		}
-
-		return cfg.JwtSecret, nil
-	})
-
-	if err != nil {
-		log.Println("error checking: ", err)
-	}
-
-	claims, ok := parsedToken.Claims.(*jwt.MapClaims)
-	if !ok || !parsedToken.Valid {
-		return nil, fmt.Errorf("%s: %w", op, ok)
-	}
-
-	return claims, nil
-}
-
 type UserService interface {
-	Login(ctx context.Context) (string, error)
-	Logout(ctx context.Context) error
+	Login(ctx context.Context, userIn UserLogin) (string, error)
 	Register(ctx context.Context, userIn UserCreate) (uuid.UUID, error)
 }
 
-type SqliteUserService struct {
+type UserServiceImpl struct {
 	log  *slog.Logger
 	repo UserRepo
 	auth AuthService
 	cfg  *config.Security
 }
 
-func NewUserService(log *slog.Logger, repo UserRepo, auth AuthService, cfg *config.Security) *SqliteUserService {
-	return &SqliteUserService{
+func NewUserService(log *slog.Logger, repo UserRepo, auth AuthService, cfg *config.Security) *UserServiceImpl {
+	return &UserServiceImpl{
 		log:  log,
 		repo: repo,
 		auth: auth,
+		cfg:  cfg,
 	}
 }
 
-func (u *SqliteUserService) Register(ctx context.Context, userIn UserCreate) (uuid.UUID, error) {
+func (u *UserServiceImpl) Register(ctx context.Context, userIn UserCreate) (uuid.UUID, error) {
 	const op = "modules.user.service.Register"
 
 	if !ValidateEmailFormat(userIn.Email) {
@@ -127,7 +41,12 @@ func (u *SqliteUserService) Register(ctx context.Context, userIn UserCreate) (uu
 		return uuid.UUID{}, fmt.Errorf("%s", res)
 	}
 
-	userIn.Pass = u.auth.HashPass(userIn.Pass)
+	pass, err := u.auth.HashPass(userIn.Pass)
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	userIn.Pass = pass
 
 	id, err := u.repo.Create(ctx, userIn)
 	if err != nil {
@@ -138,10 +57,10 @@ func (u *SqliteUserService) Register(ctx context.Context, userIn UserCreate) (uu
 	return id, nil
 }
 
-func (u *SqliteUserService) Login(ctx context.Context, userIn UserLogin) (string, error) {
+func (u *UserServiceImpl) Login(ctx context.Context, userIn UserLogin) (string, error) {
 	const op = "modules.user.service.Login"
 
-	user, err := u.repo.GetByEmail(ctx, userIn.Email)
+	user, err := u.repo.GetInfoByEmail(ctx, userIn.Email)
 	if err != nil {
 		return "", fmt.Errorf("%s: %w", op, err)
 	}
@@ -155,15 +74,11 @@ func (u *SqliteUserService) Login(ctx context.Context, userIn UserLogin) (string
 		return "", fmt.Errorf("wrong password")
 	}
 
-	token, err := u.auth.GenJwtToken(userIn)
+	token, err := u.auth.GenJwtToken(UserLoginResp{
+		Email:       user.Email,
+		Pass:        user.Pass,
+		AccessLevel: user.AccessLevel,
+	})
 
 	return token, nil
-}
-
-func (u *SqliteUserService) Logout(ctx context.Context) error {
-	const op = "modules.user.service.Logout"
-
-	// Add logout logic
-
-	return nil
 }
